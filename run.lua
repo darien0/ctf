3bf06a89e8daca88a3b3ca94654987b4272a1b7c
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-01-04 14:27:22 -0500

    Forget about swig, wrap cow by hand

--- run.lua
+++ run.lua
@@ -2,14 +2,16 @@
 local buffer = require 'buffer'
 local MPI = require 'MPI'
 local H5 = require 'HDF5'
-local GL = require 'luagl'
-local GLUT = require 'luaglut'
 local cow = require 'cow'
 
+MPI.Init()
 
-print(MPI)
-print(H5)
-print(buffer)
-print(cow)
+local domain = cow.domain_new()
 
---for k,v in pairs(cow) do print(k,v) end
+cow.domain_setndim(domain, 1)
+cow.domain_setsize(domain, 0, 100)
+
+cow.domain_commit(domain)
+cow.domain_del(domain)
+
+MPI.Finalize()
