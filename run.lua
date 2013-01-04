8e1e1733eaaa93d01da2d143cb4df36104782a13
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-01-04 14:32:50 -0500

    Update domain_getcomm function

--- run.lua
+++ run.lua
@@ -1,4 +1,5 @@
 
+
 local buffer = require 'buffer'
 local MPI = require 'MPI'
 local H5 = require 'HDF5'
@@ -6,12 +7,19 @@ local cow = require 'cow'
 
 MPI.Init()
 
+local crank = buffer.new_buffer(buffer.sizeof(buffer.int))
+
 local domain = cow.domain_new()
+local comm = MPI.Comm()
 
 cow.domain_setndim(domain, 1)
 cow.domain_setsize(domain, 0, 100)
 
 cow.domain_commit(domain)
+
+cow.domain_getcomm(domain, comm)
+MPI.Comm_rank(comm, crank)
+
 cow.domain_del(domain)
 
 MPI.Finalize()
