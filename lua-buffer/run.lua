e3dc3789d00f5d1949d1b4dfaa27cc17e5d43bf4
Jonathan Zrake <jonathan.zrake@gmail.com>
2013-01-12 14:09:05 -0500

    Allow assignment to array subsets

--- lua-buffer/run.lua
+++ lua-buffer/run.lua
@@ -10,20 +10,6 @@ local V1 = A1:vector()
 
 for i=0,#V1-1 do V1[i] = i end
 
-local exten0 = array.vector(A0:extent(), 'int'):buffer()
-local exten1 = array.vector(A1:extent(), 'int'):buffer()
+A0[{{2,8},{2,8},{2,8}}] = A1[{{2,8},{2,8},{2,8}}]
 
-local start0 = array.vector({0,0,0}, 'int'):buffer()
-local start1 = array.vector({0,0,0}, 'int'):buffer()
-
-local strid0 = array.vector({1,1,1}, 'int'):buffer()
-local strid1 = array.vector({1,1,1}, 'int'):buffer()
-
-local count0 = array.vector({10,10,10}, 'int'):buffer()
-local count1 = array.vector({10,10,10}, 'int'):buffer()
-
-print(V0)
-buffer.copy(A0:buffer(), exten0, start0, strid0, count0,
-	    A1:buffer(), exten1, start1, strid1, count1,
-	    buffer.sizeof(buffer[A0:dtype()]))
-print(V0)
+for i=0,#V0-1 do print(V0[i]) end
